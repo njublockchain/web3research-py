ea49f5041dbdb3b352bbfe4942f9cbe95090661c
Command <maoxs2@gmail.com>
2023-08-31 19:14:17 +0800

    combine utils with Kylink

--- kylink/kylink.py
+++ kylink/kylink.py
@@ -1,10 +1,37 @@
+import os
+import base64
+from io import BytesIO
+
 from kylink.defi import DeFiProvider
 from kylink.market import MarketProvider
 from kylink.gql import GraphQLProvider
 from kylink.resolve import ResolveProvider
 from kylink.token import TokenProvider
 from kylink.wallet import WalletProvider
-from kylink.utils import Utils
+
+import matplotlib.pyplot as plt
+
+
+# Patch
+def ensure_matplotlib_patch():
+    # _old_show = plt.show
+
+    def show():
+        buf = BytesIO()
+        plt.savefig(buf, format="png")
+        buf.seek(0)
+        # Encode to a base64 str
+        img = "data:image/png;base64," + base64.b64encode(buf.read()).decode("utf-8")
+        # Write to stdout
+        print(img)
+        plt.clf()
+
+    plt.show = show
+
+
+os.environ["MPLBACKEND"] = "AGG"
+
+ensure_matplotlib_patch()
 
 
 class Kylink:
@@ -16,3 +43,9 @@ class Kylink:
         self.wallet = WalletProvider(self.raw)
         self.token = TokenProvider(self.raw)
         self.utils = Utils()
+
+    def install(self):
+        ensure_matplotlib_patch()
+
+    def table(self, table_element_list):
+        print("data:table/" + json.dumps(table_element_list))
