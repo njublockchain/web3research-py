fa596a67c38cad433461be730f9a93baa1b85e98
Command <me@c0mm4nd.com>
2023-09-22 10:20:21 +0800

    update display methods

--- kylink/kylink.py
+++ kylink/kylink.py
@@ -13,7 +13,12 @@ class Kylink:
         # Set this _before_ importing matplotlib
         os.environ["MPLBACKEND"] = "AGG"
 
-    def image(self, plt):
+    def plotly(self, plot):
+        # Encode to a base64 str
+        html = "data:kylink/html;base64," + base64.b64encode(plot.to_html()).decode("utf-8")
+        print(html)
+
+    def matplotlib(self, plt):
         buf = BytesIO()
         plt.savefig(buf, format="png")
         buf.seek(0)
@@ -24,4 +29,4 @@ class Kylink:
         plt.clf()
 
     def table(self, table_element_list):
-        print("data:table/" + json.dumps(table_element_list))
+        print("data:kylink/table;json," + json.dumps(table_element_list))
