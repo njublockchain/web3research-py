04bf2179941c6fd4741703d62c8d821ce7ad1304
Command <me@c0mm4nd.com>
2024-08-10 01:32:45 +0800

    feat: Add Address and Hash classes

---
+++ web3research/common/types.py
@@ -0,0 +1,34 @@
+class Address:
+    def __init__(self, addr: str):
+        assert addr.startswith("0x"), f"Address must start with 0x, got {addr}"
+        self.addr = addr
+
+    def __repr__(self):
+        return f"unhex('{self.addr.removeprefix('0x')}')"
+
+    def __str__(self):
+        return self.addr
+
+    def __eq__(self, other):
+        return self.addr == other.addr
+
+    def __hash__(self):
+        return self.addr
+
+
+class Hash:
+    def __init__(self, hash: str):
+        assert hash.startswith("0x"), f"Hash must start with 0x, got {hash}"
+        self.hash = hash
+
+    def __repr__(self):
+        return f"unhex('{self.hash.removeprefix('0x')}')"
+
+    def __str__(self):
+        return self.hash
+
+    def __eq__(self, other):
+        return self.hash == other.hash
+
+    def __hash__(self):
+        return self.hash
