1ef41970c8313f2bb972835f97296bdaacc5e5b6
Command <me@c0mm4nd.com>
2024-08-10 02:26:39 +0800

    refactor: remove __str__ for Hash and Address

--- web3research/common/types.py
+++ web3research/common/types.py
@@ -3,11 +3,8 @@ class Address:
         assert addr.startswith("0x"), f"Address must start with 0x, got {addr}"
         self.addr = addr
 
-    def __str__(self):
-        return f"unhex('{self.addr.removeprefix('0x')}')"
-
     def __repr__(self):
-        return self.addr
+        return f"unhex('{self.addr.removeprefix('0x')}')"
 
     def __eq__(self, other):
         return self.addr == other.addr
@@ -21,11 +18,8 @@ class Hash:
         assert hash.startswith("0x"), f"Hash must start with 0x, got {hash}"
         self.hash = hash
 
-    def __str__(self):
-        return f"unhex('{self.hash.removeprefix('0x')}')"
-
     def __repr__(self):
-        return self.hash
+        return f"unhex('{self.hash.removeprefix('0x')}')"
 
     def __eq__(self, other):
         return self.hash == other.hash
