ce020aa0757605f08190905c05e1f9cabc90bc76
Command <me@c0mm4nd.com>
2024-08-12 20:48:03 +0800

    feat: add ERC ABIs

--- web3research/evm/__init__.py
+++ web3research/evm/__init__.py
@@ -1,1 +1,10 @@
-from .decoder import *
+from .decoder import SingleEventDecoder, ContractDecoder
+from .abi import ERC20_ABI, ERC721_ABI, ERC1155_ABI
+
+__all__ = [
+    "SingleEventDecoder",
+    "ContractDecoder",
+    "ERC20_ABI",
+    "ERC721_ABI",
+    "ERC1155_ABI",
+]
