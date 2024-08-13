453e4d774256b8196ff5bdbe47a9b2c05f54cf51
Command <me@c0mm4nd.com>
2024-08-13 22:02:59 +0800

    refactor: Remove unused plotting methods in Web3Research

--- web3research/web3research.py
+++ web3research/web3research.py
@@ -47,36 +47,3 @@ class Web3Research:
             **kwargs,
         )
 
-    def install(self):
-        # Set this _before_ importing matplotlib
-        os.environ["MPLBACKEND"] = "AGG"
-
-    def plotly(self, plot):
-        # Encode to a base64 str
-        html = "data:w3r/html;base64," + base64.b64encode(plot.to_html()).decode(
-            "utf-8"
-        )
-        print(html)
-
-    def matplotlib(self, plt):
-        buf = BytesIO()
-        plt.savefig(buf, format="png")
-        buf.seek(0)
-        # Encode to a base64 str
-        img = "data:image/png;base64," + base64.b64encode(buf.read()).decode("utf-8")
-        # Write to stdout
-        print(img)
-        plt.clf()
-
-    def table(self, table_element_list):
-        print("data:w3r/table;json," + json.dumps(table_element_list))
-
-    def image(self, plt):
-        buf = BytesIO()
-        plt.savefig(buf, format="png")
-        buf.seek(0)
-        # Encode to a base64 str
-        img = "data:image/png;base64," + base64.b64encode(buf.read()).decode("utf-8")
-        # Write to stdout
-        print(img)
-        plt.clf()
